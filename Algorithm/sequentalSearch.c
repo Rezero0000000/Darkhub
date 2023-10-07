fdff894b9056d737854018279dfc90ae371f2984
Rekun <hikarisakata91@gmail.com>
2023-10-07 13:49:24 +0700

    Sequental Search

--- Algorithm/sequentalSearch.c
+++ Algorithm/sequentalSearch.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main () {
-  int arr[] = {1, 2,3, 7, 8, 4,5};
+  int arr[] = {1, 2, 3, 7, 8, 4,5};
   
   for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
     if (arr[i] == 7){
-      printf("7 is on %dth index of array", i);
+      printf("Element 7 found at index %d", i);
     }
   }
 
